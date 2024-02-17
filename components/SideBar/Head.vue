00afa0fd40c9748ab06c0cf1fe3ba7c90fdee6a4
岩浆块Magma <magmablock@qq.com>
2024-02-18 02:00:39 +0800

    增加了一个什么也没有的首页；调整 404 条目

--- components/SideBar/Head.vue
+++ components/SideBar/Head.vue
@@ -2,8 +2,14 @@
   <div
     class="h-16 flex items-center gap-2 font-semibold text-base text-zinc-700 select-none"
   >
-    <img class="inline-block h-10" src="~/assets/ria-logo.png" alt="RIA Logo" />
-    <span>RIA 手册</span>
+    <NuxtLink to="/">
+      <img
+        class="inline-block h-10"
+        src="~/assets/ria-logo.png"
+        alt="RIA Logo"
+      />
+    </NuxtLink>
+    <NuxtLink to="/">RIA 手册</NuxtLink>
     <span>·</span>
     <span class="opacity-80">{{ bookName }}</span>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>index.vue</div>
+  <div class="w-full h-screen grid place-items-center">
+    <NResult
+      status="404"
+      title="你好，这是首页"
+      description="但这里还没有任何内容"
+    >
+      <template #footer>
+        <NuxtLink to="/wiki/了解RIA/新人引导页">
+          <NButton> 点我去看看手册 </NButton>
+        </NuxtLink>
+      </template>
+    </NResult>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
